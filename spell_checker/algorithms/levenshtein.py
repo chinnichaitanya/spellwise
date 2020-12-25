4e8fa539ba7852df30593dff78d6dc81a1f53c6a
Chinni Chaitanya <chchaitanya95@gmail.com>
2020-12-25 11:24:33 +0530

    ✨ Refactor Levenshtein algorithm

--- spell_checker/algorithms/levenshtein.py
+++ spell_checker/algorithms/levenshtein.py
@@ -1,17 +1,10 @@
-from ..dictionary import Dictionary
 from ..utils import sort_list
+from .base import Base
 
 
-class Levenshtein(object):
-    def __init__(self, dictionary):
-        if type(dictionary) is not Dictionary:
-            raise TypeError(
-                "Expected `dictionary` to be of type {}. Received {}".format(
-                    Dictionary.__name__, type(dictionary)
-                )
-            )
-
-        self.dictionary = dictionary
+class Levenshtein(Base):
+    def __init__(self):
+        super(Levenshtein, self).__init__()
 
     def _replace(self, a, b):
         if a == b:
@@ -19,40 +12,40 @@ class Levenshtein(object):
         return 1
 
     def get_suggestions(self, query_word, max_distance=2):
+        processed_query_word = self._pre_process(query_word)
+
         def search(dictionary_node, previous_row):
             for current_source_letter in dictionary_node.children:
                 current_row = [previous_row[0] + 1]
 
-                for i in range(1, len(query_word) + 1):
+                for i in range(1, len(processed_query_word) + 1):
                     value = min(
                         previous_row[i] + 1,
                         current_row[i - 1] + 1,
                         previous_row[i - 1]
-                        + self._replace(current_source_letter, query_word[i - 1]),
+                        + self._replace(
+                            current_source_letter, processed_query_word[i - 1]
+                        ),
                     )
                     current_row.append(value)
 
                 if (
                     current_row[-1] <= max_distance
-                    and dictionary_node.children[current_source_letter].word_at_node
+                    and dictionary_node.children[current_source_letter].words_at_node
                     is not None
                 ):
-                    suggestions.append(
-                        {
-                            "word": dictionary_node.children[
-                                current_source_letter
-                            ].word_at_node,
-                            "distance": current_row[-1],
-                        }
-                    )
+                    for word in dictionary_node.children[
+                        current_source_letter
+                    ].words_at_node:
+                        suggestions.append({"word": word, "distance": current_row[-1]})
 
                 if min(current_row) <= max_distance:
                     search(dictionary_node.children[current_source_letter], current_row)
 
         suggestions = list()
 
-        first_row = range(0, len(query_word) + 1)
-        search(self.dictionary, first_row)
+        first_row = range(0, len(processed_query_word) + 1)
+        search(self._dictionary, first_row)
 
         suggestions = sort_list(suggestions, "distance")
         return suggestions
