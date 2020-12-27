035811ef425a80f511bf5492c6e1576672b5998a
Chinni Chaitanya <chchaitanya95@gmail.com>
2020-12-27 21:06:57 +0530

    ♻️ Add __all__ to algorithms module

--- spell_checker/algorithms/__init__.py
+++ spell_checker/algorithms/__init__.py
@@ -1,5 +1,7 @@
 # flake8: noqa
 
+__all__ = ["CaverphoneOne", "CaverphoneTwo", "Editex", "Levenshtein", "Typox"]
+
 from .caverphone_one import CaverphoneOne
 from .caverphone_two import CaverphoneTwo
 from .editex import Editex
