b74188c071963bb86f525c9249aeb7b56c1785d7
Chinni Chaitanya <chchaitanya95@gmail.com>
2020-12-30 23:11:40 +0530

    🔧 Add setup.py file

---
+++ setup.py
@@ -0,0 +1,53 @@
+import setuptools
+
+with open("README.md", mode="r") as fd:
+    long_description = fd.read()
+
+setuptools.setup(
+    name="spellwise",
+    version="0.5.1",
+    author="Chaitanya Chinni",
+    description="🚀 Extremely fast spelling checker and suggester in Python!",
+    license="MIT License",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chinnichaitanya/spellwise",
+    packages=setuptools.find_packages(),
+    install_requires=["typing;python_version<'3.5'"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Healthcare Industry",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.0",
+        "Programming Language :: Python :: 3.1",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: Implementation",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires=">=3",
+    keywords=[
+        "natural language processing",
+        "nlp",
+        "spelling correction",
+        "edit-distance",
+        "levenshtein distance",
+        "editex",
+        "caverphone",
+        "typox",
+    ],
+)
