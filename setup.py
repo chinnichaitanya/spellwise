88293d6f19d01ef1cfca83584cc08e9dea877fc8
Chinni Chaitanya <chchaitanya95@gmail.com>
2021-02-07 20:58:00 +0530

    🔧 Bump spellwise version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", mode="r") as fd:
 
 setuptools.setup(
     name="spellwise",
-    version="0.6.2",
+    version="0.7.0",
     author="Chaitanya Chinni",
     description="🚀 Extremely fast spelling checker and suggester in Python!",
     license="MIT License",
