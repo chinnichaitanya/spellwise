7b3c8f46330c1111facab6ce928f556271c5e730
Chinni Chaitanya <chchaitanya95@gmail.com>
2021-02-07 17:10:22 +0530

    🔧 Bump spellwise version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", mode="r") as fd:
 
 setuptools.setup(
     name="spellwise",
-    version="0.5.2",
+    version="0.6.2",
     author="Chaitanya Chinni",
     description="🚀 Extremely fast spelling checker and suggester in Python!",
     license="MIT License",
